09f7eb8fcca06c14a43148dd0d9bcb15e41a3338
Hüseyin <hkayabilisim@gmail.com>
2016-10-05 20:54:03 -0400

    Initial Commit

---
+++ msm4g/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  msm4g
+//
+//  Created by Huseyin Kaya on 05/10/16.
+//  Copyright © 2016 Huseyin Kaya. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
