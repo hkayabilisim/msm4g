ee03ec3d9608594f38db10bd0375a3774bd628d4
hkayabilisim <hkayabilisim@gmail.com>
2018-08-18 13:21:23 +0300

    Add a generic definition of B-Splines
    By using the recursive definition in Section II.B.1 of  "The Journal of Chemical Physics 144, 114112 (2016); doi: 10.1063/1.4943868", B-Splines and their derivatives can now be evaluated for any order.

--- msm4g_bases.c
+++ msm4g_bases.c
@@ -16,4 +16,19 @@ static const struct BaseFunction _CubicBSpline = { 4, {cubic_bspline0, cubic_bsp
 const BaseFunction *CubicHermite = & _CubicHermite;
 const BaseFunction *CubicBSpline = & _CubicBSpline;
 
+double msm4g_bases_bspline(int k, double u) {
+    if (k == 1) {
+        if (u >= 0 && u < 1)
+            return 1.0;
+        else
+            return 0.0;
+    } else {
+        return u / (double) (k - 1) * msm4g_bases_bspline(k - 1, u)
+        + (k - u) / (double) (k - 1) * msm4g_bases_bspline(k - 1, u - 1.0);
+    }
+}
 
+/* From Section III.B.1 of Bob's B-spline article */
+double msm4g_bases_bsplineprime(int k, double u) {
+    return msm4g_bases_bspline(k - 1, u) - msm4g_bases_bspline(k - 1, u - 1);
+}
--- msm4g_bases.h
+++ msm4g_bases.h
@@ -14,6 +14,36 @@ typedef struct BaseFunction
 extern const BaseFunction *CubicHermite;
 extern const BaseFunction *CubicBSpline;
 
+/** @brief Evaluates B-Spline bases
+ *
+ * Calculates the B-Spline bases of order k at any given 
+ * point u. It uses the recursive definition given in
+ * Eq 7 in "The Journal of Chemical Physics 144, 114112 (2016); doi: 10.1063/1.4943868".
+ * 
+ * @todo Using the recursive definition is easy and appearently not too costly
+ * but it is one of the first places to look for if overall MSM has some performance
+ * issues.
+ *
+ * @param[in] k Order of B-Spline
+ * @param[in] u Evaluation point
+ * @return B-Spline of order k evaluated at point u
+ */
+double msm4g_bases_bspline(int k,double u);
 
+/** @brief Evaluates the derivative B-Spline bases
+ *
+ * Calculates the derivative of B-Spline bases of order k at any given
+ * point u. It uses the recursive definition given in
+ * Section II.B.1 in "The Journal of Chemical Physics 144, 114112 (2016); doi: 10.1063/1.4943868".
+ *
+ * @todo Like its non-derivative version, it also uses a recursive definition.
+ * So it is also one of the first places to look for if overall MSM has some performance
+ * issues.
+ *
+ * @param[in] k Order of B-Spline
+ * @param[in] u Evaluation point
+ * @return Derivative of B-Spline of order k evaluated at point u
+ */
+double msm4g_bases_bsplineprime(int k,double u);
 
 #endif /* msm4g_bases_h */
